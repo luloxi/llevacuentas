06818731db2b1671f44302501de774c6931af8be
Lulox <lulox.eth@protonmail.com>
2026-08-06 13:34:19 -0300

    feat: register SW in app shell for share target

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -2,6 +2,7 @@ import { Suspense } from "react";
 import { AppNav } from "@/components/app-nav";
 import { AddExpenseProvider } from "@/components/add-expense-provider";
 import { SectionSwipe } from "@/components/section-swipe";
+import { RegisterServiceWorker } from "@/components/register-sw";
 import { requireUser } from "@/lib/session";
 import { isAdminEmail } from "@/lib/auth/allowlist";
 
@@ -16,6 +17,7 @@ export default async function AppLayout({
 
   return (
     <div className="app-shell flex min-h-dvh flex-col">
+      <RegisterServiceWorker />
       <AddExpenseProvider>
         <AppNav isAdmin={isAdminEmail(user.email)} />
         <main className="mx-auto flex w-full max-w-6xl flex-1 flex-col px-4 py-6 pb-24 md:px-6 md:pb-10">
